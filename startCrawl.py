30cb850456aca311ad8024e8d61bbff8324126f7
Nathan <nthuff@gmail.com>
2014-05-30 18:04:01 -0600

    End of day

--- startCrawl.py
+++ startCrawl.py
@@ -15,6 +15,8 @@ lyricHref = re.compile("id=\"lyrics-body-text\">(.*?)</div>")
 cleanHTML = re.compile("<.*?>")
 cleanSpace = re.compile("\+\++")
 words = {} #word:count
+googleUrl = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
+metroUrl = "http://www.metrolyrics.com/search.html?search="
 
 
 
@@ -30,13 +32,15 @@ def main(numPages, args):
 			billQ.put("%s%s" % (url, i))
 
 	billSearch = Process(target=grabBody, args=(billQ,googleQ))
-	googSearch = Process(target=googleSearch, args=(googleQ, lyricQ))
+#	googSearch = Process(target=googleSearch, args=(googleQ, lyricQ))
+	metSearch = Process(target=metroSearch, args=(googleQ, lyricQ))
 	lyrSearch = Process(target=lyricSearch, args=(lyricQ, googleQ, wordsQ))
 	checkPt = Process(target=checkPoint, args=(billQ, googleQ, lyricQ, wordsQ))
 	
 	billSearch.start()
-	sleep(10)
-	googSearch.start()
+	sleep(2)
+	#googSearch.start()
+	metSearch.start()
 	lyrSearch.start()
 	checkPt.start()
 	
@@ -50,17 +54,34 @@ def grabBody(billQ, googleQ):
 			billQ.put(url)
 		for link in  y:
 			print "grabBody ", link
-			link = (link[0], link[1], "site:www.metrolyrics.com")
+			link = (link[0], link[1])
 			googleQ.put(" ".join(link))
 	print "!!STATUS!! billQ DONE"
 	
+
+def metroSearch(googleQ, lyricQ):
+	metroLyricGrab = re.compile("<span>Songs</span>.*?href=\"(.*?\.html)\"")
+	delayTime = 5
+	while not googleQ.empty():
+		origQuery = googleQ.get()
+		print "orig: ", (1, origQuery)
+		sleep(int(delayTime))
+		query = "+".join(origQuery.split())
+		print(metroUrl + query )
+		response = " ".join(urllib2.urlopen(metroUrl + query ).read().split("\n"))
+		y = metroLyricGrab.findall(response)
+		for link in y:
+			print "#####",
+			print link
+			lyricQ.put(link)
+
+
 
 def googleSearch(googleQ, lyricQ):
 	delayTime = 5
 	while not googleQ.empty():
 		origQuery = googleQ.get()
 		sleep(int(delayTime))
-		url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
 		
 		query = urllib.urlencode( {'q' : origQuery } )
 		print query
@@ -68,7 +89,7 @@ def googleSearch(googleQ, lyricQ):
 		print query
 		
 		try:
-			response = urllib2.urlopen(url + query ).read()
+			response = urllib2.urlopen(googleUrl + query ).read()
 		
 			data = json.loads( response )
 		
